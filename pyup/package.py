71f6754244409fb34ec38c68cb615340d8cf1457
Jay <ja.geb@me.com>
2015-12-01 11:05:32 +0100

    prototyping

--- pyup/package.py
+++ pyup/package.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
-
+from pkg_resources import parse_version
 
 class Package(object):
     def __init__(self, name):
@@ -11,7 +11,17 @@ class Package(object):
     def versions(self):
         if self._versions is None:
             self._complete_lazy()
-        return self.versions
+        return self._versions
 
     def _complete_lazy(self):
         self._versions = ["1.2"]
+
+    def latest_version(self, prereleases=False):
+        for version in self.versions:
+            if prereleases or not parse_version(version).is_prerelease:
+                return version
+        # we have not found a version here, we might find one in prereleases
+        if self.versions and not prereleases:
+            return self.latest_version(prereleases=True)
+        return None
+
