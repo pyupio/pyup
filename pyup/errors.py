e5cfdcf2fd7bcd177406fb70213e22beabcb3fbe
Jay <ja.geb@me.com>
2015-12-01 16:46:21 +0100

    added first error classes

--- pyup/errors.py
+++ pyup/errors.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-class BranchExists(Exception):
+class BranchExistsError(Exception):
+    pass
+
+class NoPermissionError(Exception):
     pass
