10f7fcafa4dff4471e749c1697e1cc784a0952fc
Jay <ja.geb@me.com>
2015-12-06 12:53:41 +0100

    no need to test base exception classes

--- pyup/errors.py
+++ pyup/errors.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals
 
-
-class BranchExistsError(Exception):
+class BranchExistsError(Exception):  # pragma: no cover
     pass
 
 
-class NoPermissionError(Exception):
+class NoPermissionError(Exception):  # pragma: no cover
     pass
