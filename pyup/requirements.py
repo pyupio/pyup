439dd5b988c59fbda7d6dee144f1bd78b28e85e4
Jannis Gebauer <ja.geb@me.com>
2016-08-24 09:45:11 +0200

    make private index server parsing more robust

--- pyup/requirements.py
+++ pyup/requirements.py
@@ -7,6 +7,40 @@ from .updates import InitialUpdate, SequentialUpdate
 from .pullrequest import PullRequest
 import logging
 from .package import Package, fetch_package
+import re
+# see https://gist.github.com/dperini/729294
+URL_REGEX = re.compile(
+    # protocol identifier
+    u"(?:(?:https?|ftp)://)"
+    # user:pass authentication
+    u"(?:\S+(?::\S*)?@)?"
+    u"(?:"
+    # IP address exclusion
+    # private & local networks
+    u"(?!(?:10|127)(?:\.\d{1,3}){3})"
+    u"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
+    u"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
+    # IP address dotted notation octets
+    # excludes loopback network 0.0.0.0
+    # excludes reserved space >= 224.0.0.0
+    # excludes network & broadcast addresses
+    # (first & last IP address of each class)
+    u"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
+    u"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
+    u"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
+    u"|"
+    # host name
+    u"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
+    # domain name
+    u"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
+    # TLD identifier
+    u"(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
+    u")"
+    # port number
+    u"(?::\d{2,5})?"
+    # resource path
+    u"(?:/\S*)?"
+    , re.UNICODE)
 
 logger = logging.getLogger(__name__)
 
@@ -82,12 +116,11 @@ class RequirementFile(object):
 
     @staticmethod
     def parse_index_server(line):
-        try:
-            line = line.split("#")[0]  # removes comments
-            url = line.split(" ")[1].strip()
+        matches = URL_REGEX.findall(line)
+        if matches:
+            url = matches[0]
             return url if url.endswith("/") else url + "/"
-        except IndexError:
-            return None
+        return None
 
     def _parse(self):
         self._requirements, self._other_files = [], []
--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -517,6 +517,42 @@ class RequirementsFileTestCase(TestCase):
             None
         )
 
+        line = "--index-url=https://some.foo/"
+        self.assertEqual(
+            RequirementFile.parse_index_server(line),
+            "https://some.foo/"
+        )
+
+        line = "-i=https://some.foo/"
+        self.assertEqual(
+            RequirementFile.parse_index_server(line),
+            "https://some.foo/"
+        )
+
+        line = "--extra-index-url=https://some.foo/"
+        self.assertEqual(
+            RequirementFile.parse_index_server(line),
+            "https://some.foo/"
+        )
+
+        line = "--extra-index-url=https://some.foo"
+        self.assertEqual(
+            RequirementFile.parse_index_server(line),
+            "https://some.foo/"
+        )
+
+        line = "--extra-index-url=https://some.foo # some lousy comment"
+        self.assertEqual(
+            RequirementFile.parse_index_server(line),
+            "https://some.foo/"
+        )
+
+        line = "-i\t\t =https://some.foo \t\t    # some lousy comment"
+        self.assertEqual(
+            RequirementFile.parse_index_server(line),
+            "https://some.foo/"
+        )
+
     @patch("pyup.requirements.Requirement.package")
     def test_parse_package_with_index_server(self, package):
         content = """-i https://some.foo/\ndjango"""
