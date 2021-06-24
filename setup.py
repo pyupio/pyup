3bebe387b66a81567f9be45fa285acb07016e6db
Damian Zaremba <damian@damianzaremba.co.uk>
2021-06-24 14:03:43 +0200

    Move minimum Python version to 3.6
    Python 3.5 has been EOL since Sept 2020, 10 months ago at this point.
    
    The latest release of PyGithub does not support 3.5 (#1770), thus remove
    the support here also.
    
    Also add test cases for Python 3.9, which is the current stable release.

--- setup.py
+++ setup.py
@@ -54,13 +54,13 @@ setup(
         'Natural Language :: English',
         "Programming Language :: Python",
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     entry_points={
         'console_scripts': [
             'pyup = pyup.cli:main',
